4b2abccdb0b7fbe06847e4fb9cd1da7213145b24
vagrant <vagrant@homestead>
2025-09-11 08:24:45 -0500

    migrazione nuovi progetti e nuova mappa

--- src/eucitizensciencetheme/urls.py
+++ src/eucitizensciencetheme/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path("final_event/", views.final_event, name="final_event"),
     path("final_launch/", views.final_launch, name="final_launch"),
     path('get_projects/', views.get_projects, name='get_projects'),
+    path('get_projects_webmapp/', views.get_projects_webmapp, name='get_projects_webmapp'),
     path('get_organisations/', views.get_organisations, name='get_organisations'),
     path("imprint/", views.imprint, name="imprint"),
     path("moderation/", views.moderation, name="moderation"),
