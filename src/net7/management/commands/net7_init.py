0b684a53320c0194437a6bf659f87e263a40b14d
Giacomo Terreni <giacomo.terreni@gmail.com>
2025-10-06 12:16:58 +0200

    topbar

--- src/net7/management/commands/net7_init.py
+++ src/net7/management/commands/net7_init.py
@@ -165,7 +165,7 @@ class Command(BaseCommand):
         TopBar.objects.create(
             name='bdsweek_map',
             slug='/bdsweek_map',
-            name_it='BS Week',
+            name_it='BSW',
             position=5
         )
         TopBar.objects.create(
