57c44ee8a63f1721052bd60a91d98155ad0eaebf
vagrant <vagrant@homestead>
2025-10-09 05:38:50 -0500

    aggiunto modulo cors

--- src/eucs_platform/settings/base.py
+++ src/eucs_platform/settings/base.py
@@ -122,6 +122,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     "net7",
+    "corsheaders",
     "modeltranslation",
     "eucitizensciencetheme",
     "django.contrib.auth",
@@ -205,6 +206,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'eucitizensciencetheme.middleware.TopBarMiddleware',
     'eucitizensciencetheme.middleware.FooterMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     # Machina
     'machina.apps.forum_permission.middleware.ForumPermissionMiddleware',
     # TopBar
@@ -613,6 +615,7 @@ CRONJOBS = [
     ('0 * * * *', 'eucs_platform.cron.NewForumResponseCronJob')
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 GRAPH_MODELS = {
     'all_applications': True,
