2054c36b77723f001f2d82da801cf81ffc18f794
Giacomo Terreni <giacomo.terreni@gmail.com>
2025-11-07 15:11:50 +0100

    organisation fotos

--- src/organisations/views.py
+++ src/organisations/views.py
@@ -109,32 +109,33 @@ def saveImage(request, form, element, ref):
         if not image:
             return None
 
-        cropped_image = image.crop((x, y, w+x, h+y))
-        if (ref == '3'):
-            finalSize = (1100, 400)
-        else:
-            finalSize = (600, 400)
+        #cropped_image = image.crop((x, y, w+x, h+y))
+        # if (ref == '3'):
+        #     finalSize = (1100, 400)
+        # else:
+        #     finalSize = (600, 400)
 
-        resized_image = cropped_image.resize(finalSize, Image.Resampling.LANCZOS)
-
-        if (cropped_image.width > image.width):
-            size = (abs(int(
-                (finalSize[0]-(finalSize[0]/cropped_image.width*image.width))/2)), finalSize[1])
-            whitebackground = Image.new(
-                mode='RGBA', size=size, color=(255, 255, 255, 0))
-            position = ((finalSize[0] - whitebackground.width), 0)
-            resized_image.paste(whitebackground, position)
-            position = (0, 0)
-            resized_image.paste(whitebackground, position)
-        if (cropped_image.height > image.height):
-            size = (finalSize[0], abs(
-                int((finalSize[1]-(finalSize[1]/cropped_image.height*image.height))/2)))
-            whitebackground = Image.new(
-                mode='RGBA', size=size, color=(255, 255, 255, 0))
-            position = (0, (finalSize[1] - whitebackground.height))
-            resized_image.paste(whitebackground, position)
-            position = (0, 0)
-            resized_image.paste(whitebackground, position)
+        resized_image = image
+        # resized_image = cropped_image.resize(finalSize, Image.Resampling.LANCZOS)
+        #
+        # if (cropped_image.width > image.width):
+        #     size = (abs(int(
+        #         (finalSize[0]-(finalSize[0]/cropped_image.width*image.width))/2)), finalSize[1])
+        #     whitebackground = Image.new(
+        #         mode='RGBA', size=size, color=(255, 255, 255, 0))
+        #     position = ((finalSize[0] - whitebackground.width), 0)
+        #     resized_image.paste(whitebackground, position)
+        #     position = (0, 0)
+        #     resized_image.paste(whitebackground, position)
+        # if (cropped_image.height > image.height):
+        #     size = (finalSize[0], abs(
+        #         int((finalSize[1]-(finalSize[1]/cropped_image.height*image.height))/2)))
+        #     whitebackground = Image.new(
+        #         mode='RGBA', size=size, color=(255, 255, 255, 0))
+        #     position = (0, (finalSize[1] - whitebackground.height))
+        #     resized_image.paste(whitebackground, position)
+        #     position = (0, 0)
+        #     resized_image.paste(whitebackground, position)
 
         image_path = saveImageWithPath(resized_image, photo.name)
     elif withImage:
@@ -254,9 +255,6 @@ def organisations(request):
     orgTypes = OrganisationType.objects.all()
     totalCount = len(organisations)
 
-    homeSearchCategories = request.GET.get('homeSearchCategories')
-    #return HttpResponse(homeSearchCategories)
-
     filters = {'keywords': '', 'orgTypes': '', 'country': '', 'orderby': ''}
     """
     if request.GET.get('keywords'):
@@ -357,7 +355,7 @@ def organisations(request):
         'countriesWithContent': countriesWithContent,
         'orgTypes': orgTypes,
         'isSearchPage': True,
-        'homeSearchCategories': homeSearchCategories,
+        'homeSearchCategories': 'organisations',
         'localita': localita,
         'localita_selected': localita_selected,
         'show_search_bar': False})
