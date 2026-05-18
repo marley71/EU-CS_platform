0184b007a1a5f051cbdfa38822af70441778f8af
Giacomo Terreni <giacomo.terreni@gmail.com>
2026-05-18 10:45:19 +0200

    ultime sisgtemazioni

--- src/eucs_platform/settings/base.py
+++ src/eucs_platform/settings/base.py
@@ -403,7 +403,6 @@ SUMMERNOTE_CONFIG = {
 #EMAIL_ECSA_ADMIN = env("EMAIL_ECSA_ADMIN", default="").split(",")
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env("HOST_EMAIL")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
@@ -411,8 +410,8 @@ EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = env('EMAIL_PORT')
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = True
-EMAIL_BACKEND = 'django_ses.SESBackend'
-#EMAIL_BACKEND = "eucs_platform.mail_backends.Py312SMTPBackend"
+#EMAIL_BACKEND = 'django_ses.SESBackend'
+EMAIL_BACKEND = "eucs_platform.mail_backends.Py312SMTPBackend"
 DEFAULT_FROM_EMAIL = env("FROM_EMAIL", default="")
 EMAIL_RECIPIENT_LIST = env("EMAIL_RECIPIENT_LIST", default="").split(",")
 EMAIL_CONTACT_RECIPIENT_LIST = env("EMAIL_CONTACT_RECIPIENT_LIST", default="").split(",")
